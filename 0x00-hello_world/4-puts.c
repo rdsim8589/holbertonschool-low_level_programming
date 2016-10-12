be9b8be345aec3f11f8ba2520083128bc87921ed
Richard Sim <rdsim8589@gmail.com>
2016-10-12 22:10:42 +0000

    4 comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - prints
+ * @parameterx: void
+(*
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: "Programming is like building a multilingual puzzle
+ *
+ */
 int main(void)
 {
   puts ("\"Programming is like building a multilingual puzzle");
