a4b78123d2eb4cd21904ee0b0cc9eb61a5f945f6
Richard Sim <rdsim8589@gmail.com>
2016-10-19 03:46:06 +0000

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the sum of the Fibonacci numbers up to 50
+ * main - print the first 98 Fibonacci numbers
  *
  * Return: 0
  */
@@ -9,29 +9,37 @@
 int main(void)
 {
 	int n;
-	unsigned long fib1, fib2, ffib1, bfib1, ffib2, bfib2;
+	long int fib1, fib2, ffib1, bfib1, ffib2, bfib2, carry;
 
 	fib1 = 1;
 	fib2 = 2;
-	for (n = 0; n < 49; n++)
+	for (n = 0; n < 48; n++)
 	{
-		if (fib2 < 4294967295)
+		if (n < 44)
 		{
 			printf("%lu, %lu", fib1, fib2);
 			fib1 += fib2;
 			fib2 += fib1;
+			if (n == 43)
+			{
+			ffib1 = fib1 / 10000000000;
+			bfib1 = fib1 % 10000000000;
+			ffib2 = fib2 / 10000000000;
+			bfib2 = fib2 % 10000000000;
+			}
+
 		}
 		else
 		{
-			ffib1 = fib1 / 1000000000;
-			bfib1 = fib1 % 1000000000;
-			ffib2 = fib1 / 1000000000;
-			bfib2 = fib1 % 1000000000;
-			printf("%lu%lu, %lu%lu", ffib1, bfib1, ffib2, bfib2);
-			fib1 += fib2;
-			fib2 += fib1;
+			bfib1 += bfib2;
+			carry = bfib1 / 10000000000;
+			ffib1 = ffib1 + ffib2 + carry;
+			bfib2 += bfib1;
+			carry = bfib2 / 10000000000;
+			ffib2 = ffib1 + ffib2 + carry;
+			printf("%ld%ld, %ld%ld", ffib1, bfib1, ffib2, bfib2);
 		}
-		if (n != 48)
+		if (n != 47)
 		{
 			printf(", ");
 		}
