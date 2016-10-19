a7645d84d53ee4f7b50ee54f599a44dd2640d0c4
Richard Sim <rdsim8589@gmail.com>
2016-10-19 04:02:12 +0000

    16c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n;
-	long int fib1, fib2, ffib1, bfib1, ffib2, bfib2, carry;
+	long int fib1, fib2, ffib1, bfib1, ffib2, bfib2;
 
 	fib1 = 1;
 	fib2 = 2;
@@ -32,11 +32,11 @@ int main(void)
 		else
 		{
 			bfib1 += bfib2;
-			carry = bfib1 / 10000000000;
-			ffib1 = ffib1 + ffib2 + carry;
+			bfib1 = bfib1 % 10000000000;
+			ffib1 = ffib1 + ffib2 + (bfib1 / 10000000000);
 			bfib2 += bfib1;
-			carry = bfib2 / 10000000000;
-			ffib2 = ffib1 + ffib2 + carry;
+			bfib2 = bfib2 % 10000000000;
+			ffib2 = ffib1 + ffib2 + (bfib2 / 10000000000);
 			printf("%ld%ld, %ld%ld", ffib1, bfib1, ffib2, bfib2);
 		}
 		if (n != 47)
