02631c4d791d9644e8b6c48d566eccea8397f547
Richard Sim <rdsim8589@gmail.com>
2016-10-19 00:36:13 +0000

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of the Fibonacci numbers up to 50
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	int n;
+        long fib1, fib2;
+
+	fib1 = 1;
+	fib2 = 2;
+	for (n = 1; n <= 25; n++)
+	{
+		printf("%ld, %ld", fib1, fib2);
+		if (n != 25)
+		{
+			printf(", ");
+		}
+		fib1 += fib2;
+		fib2 += fib1;
+	}
+	printf("\n");
+}
