43b1d8c7f6359a68e38d5a6efc8dab9d02d6e1b1
Richard Sim <rdsim8589@gmail.com>
2016-10-19 05:38:26 +0000

    ajksdf

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,27 +17,27 @@ int main(void)
 	{
 		if (n < 44)
 		{
-			printf("%lu, %lu\n", fib1, fib2);
+			printf("%lu, %lu", fib1, fib2);
 			fib1 += fib2;
 			fib2 += fib1;
 			if (n == 43)
 			{
-			ffib1 = fib1 / 100000000000000;
-			bfib1 = fib1 % 100000000000000;
-			ffib2 = fib2 / 100000000000000;
-			bfib2 = fib2 % 100000000000000;
-			printf("%ld%ld, %ld0%ld", ffib1, bfib1, ffib2, bfib2);
+			ffib1 = fib1 / 1000000000000000;
+			bfib1 = fib1 % 1000000000000000;
+			ffib2 = fib2 / 1000000000000000;
+			bfib2 = fib2 % 1000000000000000;
+			printf(", %ld%ld, %ld0%ld", ffib1, bfib1, ffib2, bfib2);
 			}
 
 		}
 		else
 		{
 			bfib1 += bfib2;
-			ffib1 = ffib1 + ffib2 + (bfib1 / 100000000000000);
-			bfib1 = bfib1 % 100000000000000;
+			ffib1 = ffib1 + ffib2 + (bfib1 / 1000000000000000);
+			bfib1 = bfib1 % 1000000000000000;
 			bfib2 += bfib1;
-			ffib2 = ffib1 + ffib2 + (bfib2 / 100000000000000);
-			bfib2 = bfib2 % 100000000000000;
+			ffib2 = ffib1 + ffib2 + (bfib2 / 1000000000000000);
+			bfib2 = bfib2 % 1000000000000000;
 			printf("%ld%ld, %ld%ld", ffib1, bfib1, ffib2, bfib2);
 		}
 		if (n != 48)
