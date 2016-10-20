0a5cf5f1d18634eea9ca27d0ec2501170f32ae9c
Richard Sim <rdsim8589@gmail.com>
2016-10-20 21:52:09 +0000

    7 changes

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,20 @@
 void print_diagonal(int n)
 {
 	int i, j;
+
 	if (n > 0)
 	{
 		for (i = 0; i < n ; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
 				if (j == i)
 				{
 					_putchar('\\');
+				}
+				else if (j < i)
+				{
+					_putchar(' ');
 				}
 			}
 			_putchar('\n');
