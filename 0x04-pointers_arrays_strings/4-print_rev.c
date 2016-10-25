cd24a8919fde97e7ec449c8e743fb8e33bd5612c
Richard Sim <rdsim8589@gmail.com>
2016-10-25 22:36:25 +0000

    4 correction

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s_ptr)
 	int n, m;
 
 
-	for (n = 1; *(s_ptr + n) != '\0'; n++)
+	for (n = 0; s_ptr[n] != '\0'; n++)
 	{
 	}
 	for (m = n; m >= 0; m--)
 	{
-		_putchar(*(s_ptr + m));
+		_putchar(s_ptr[m]);
 	}
 	_putchar('\n');
 }
