5fc2f20920b35fa6dac2ea46db741c783e26fdef
Richard Sim <rdsim8589@gmail.com>
2016-11-03 01:03:22 +0000

    7

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -24,5 +24,41 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * int is_palindrome
+ * helper_pal - returns 1 if a string is a palindrome and 0 if not.
+ * @s: a pointer a string type
+ * @start: starting index
+ * @end: ending index
  *
+ * Return:  returns 1 if a string is a palindrome and 0 if not.
+ */
+
+int helper_pal(char *s, int start, int end)
+{
+	if (start >= end)
+	{
+		return (1);
+	}
+	if (s[start] == s[end])
+	{
+		return (helper_pal(s, start + 1, end - 1));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: a pointer a string type
+ *
+ * Return:  returns 1 if a string is a palindrome and 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	int pal_check;
+
+	if (s == '\0')
+	{
+		return (1);
+	}
+	pal_check = helper_pal(s, 0, _strlen_recursion(s) - 1);
+	return (pal_check);
+}
