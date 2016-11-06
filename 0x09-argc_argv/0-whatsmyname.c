e9a14585e2e63971797fd5e8f780a93a739b92c7
Richard Sim <rdsim8589@gmail.com>
2016-11-06 22:27:46 +0000

    0 change

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv)
 {
-        while(argc--)
-                printf("%s\n", *argv++);
-        exit(EXIT_SUCCESS);
+	while(argc--)
+		printf("%s\n", *argv++);
+	exit(EXIT_SUCCESS);
 }
