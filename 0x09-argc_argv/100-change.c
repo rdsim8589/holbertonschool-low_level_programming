d6fd9c99a2b787b137949b98113236e4bb447998
Richard Sim <rdsim8589@gmail.com>
2016-11-08 01:36:28 +0000

    100

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints the min number of coins needed to be returned
+ * @argc: count of arguments supplied to the program
+ * @argv: pointer to an array of pointers.
+ *
+ * Return: 1 is error, and 0 if it works
+ */
+int main(int argc, char **argv)
+{
+	int diff_coins[] = {25, 10, 5, 2, 1};
+	int coin_total, i, num_of_coins;
+
+	num_of_coins = 0;
+	i = 0;
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		coin_total = atoi(argv[1]);
+		while (i < 5)
+		{
+			num_of_coins += coin_total/diff_coins[i];
+			coin_total = coin_total % diff_coins[i];
+			i++;
+		}
+		printf("%d\n", num_of_coins);
+	}
+	return(0);
+}
