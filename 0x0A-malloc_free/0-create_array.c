83a4a83710be7532dbf93122c7c5bd3a007fd6ad
Richard Sim <rdsim8589@gmail.com>
2016-11-09 22:03:49 +0000

    correct 0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -15,8 +15,10 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
+	{
 		free(array);
 		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
