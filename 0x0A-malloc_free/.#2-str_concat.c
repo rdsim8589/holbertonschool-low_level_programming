4064130187edc771457496bd9025d67b10f72858
Richard Sim <rdsim8589@gmail.com>
2016-11-09 22:53:35 +0000

    change

--- 0x0A-malloc_free/.#2-str_concat.c
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.3192:1478706291
--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -28,8 +28,14 @@ char *str_concat(char *s1, char *s2)
 	int total_len, i, j, ar_i;
 	char *strs[2];
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+	{
+        	s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	strs[0] = s1;
 	strs[1] = s2;
 	total_len = _strlen(s1) + _strlen(s2) + 1;
