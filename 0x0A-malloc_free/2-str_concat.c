72774d098b33dda7bc2acbcd4e6e154bd6db5ef8
Richard Sim <rdsim8589@gmail.com>
2016-11-09 22:39:57 +0000

    2 correction

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -29,14 +29,16 @@ char *str_concat(char *s1, char *s2)
 	char *strs[2];
 
 	if (s1 == NULL || s2 == NULL)
-		return ( NULL);
+		return  NULL);
 	strs[0] = s1;
 	strs[1] = s2;
 	total_len = _strlen(s1) + _strlen(s2) + 1;
 	ar = malloc(total_len * sizeof(char));
 	if (ar == NULL)
+	{
 		free(ar);
 		return (NULL);
+	}
 	ar_i = 0;
 	for (i = 0; i < 2; i++)
 	{
