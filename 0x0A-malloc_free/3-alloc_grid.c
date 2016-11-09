3d315a0e14136ab14cc76935f0dcc98bd36aa154
Richard Sim <rdsim8589@gmail.com>
2016-11-09 03:50:22 +0000

    change 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -25,9 +25,10 @@ int **alloc_grid(int width, int height)
 		{
 			while (i >= 0)
 			{
-				free(ar[i - 1]);
+				free(ar[i]);
 				i--;
 			}
+			free(ar);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
