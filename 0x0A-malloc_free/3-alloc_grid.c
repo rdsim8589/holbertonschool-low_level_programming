90fe3b505d91072037663e60883d41871699d0b2
Richard Sim <rdsim8589@gmail.com>
2016-11-09 03:51:21 +0000

    3 test

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		ar[i] = malloc(width * sizeof(int *));
+		ar[i] = malloc(width * sizeof(int));
 		if (ar[i] == NULL)
 		{
 			while (i >= 0)
