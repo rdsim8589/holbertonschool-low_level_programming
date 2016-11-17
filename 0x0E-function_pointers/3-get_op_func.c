ce68c0580169247987bf8822d360b730452a50d4
Richard Sim <rdsim8589@gmail.com>
2016-11-17 19:01:05 +0000

    removing space in 3

--- 0x0E-function_pointers/3-get_op_func.c
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -8,7 +8,7 @@
  *
  * Return: a pointer to the function of the correct operator
  */
-int (*get_op_func(char *s)) (int, int)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
