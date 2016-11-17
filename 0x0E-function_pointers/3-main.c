72134ea28bc4a9c013ec40bd5e1da6cb098bba21
Richard Sim <rdsim8589@gmail.com>
2016-11-17 19:10:20 +0000

    removing number filter

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -18,17 +18,14 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[1][0] >= '0' && argv[1][0] <= '9'
-	    && argv[3][0] >= '0' && argv[3][0] <= '9')
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	if (get_op_func(argv[2]) == NULL ||
+	    argv[2][1] != '\0')
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[3]);
-		if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		printf("%d\n", get_op_func(argv[2]) (num1, num2));
+		printf("Error\n");
+		exit(99);
 	}
+	printf("%d\n", get_op_func(argv[2]) (num1, num2));
 	return (0);
 }
