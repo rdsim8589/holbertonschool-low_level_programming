a4b450072a430baaf5c8a68a0c4296f9f4bc5f74
Richard Sim <rdsim8589@gmail.com>
2016-11-20 04:04:22 +0000

    correcting to not print separator if NULL

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -10,12 +10,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list numbers;
 	unsigned int i;
-
-	if (separator != NULL)
 	{
 		va_start(numbers, n);
 		for (i = 0; i < n - 1; i++)
-			printf("%d%s", va_arg(numbers, int), separator);
+		{
+			if (separator != NULL)
+				printf("%d%s", va_arg(numbers, int), separator);
+			else
+				printf("%d", va_arg(numbers, int));
+		}
 		printf("%d\n", va_arg(numbers, int));
 		va_end(numbers);
 	}
