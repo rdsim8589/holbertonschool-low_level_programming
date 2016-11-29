30c2dd138c758b8d8f809146c817bda123a627d4
Richard Sim <rdsim8589@gmail.com>
2016-11-29 00:38:21 +0000

    adding solution 1-list_len.c

---
+++ 0x11-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - return the number of elements in linked list_t list
+ * @h: a const that is a pointer to list_h
+ *
+ * return: the number of elements in the linked list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	size_t i;
+
+	i = 0;
+	while (current->next != NULL)
+	{
+		current = current->next;
+		i++;
+	}
+	return(i + 1);
+
+}
