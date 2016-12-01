6e71d7818f19a1e47f645fabf660ab5016b4075a
Richard Sim <rdsim8589@gmail.com>
2016-12-01 22:59:15 +0000

    adding ex 9

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert a new node after the given index
+ * @head: a pointer to a pointer to the start of the linked list
+ * @index: the index in which a new node is inserted after the index
+ *
+ * Return: the address of the new node or
+ * NULL if it fails, or unable to add a new node.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+{
+	listint_t *new_node, *current;
+	unsigned int i;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	i = 0u;
+	current = *head;
+	while (i < index - 1)
+	{
+		if (current != NULL)
+		{
+			current = current->next;
+		}
+		else
+		{
+			return(NULL);
+		}
+		i++;
+	}
+	new_node->n = n;
+	new_node->next = current->next;
+	current->next = new_node;
+	return (new_node);
+}
