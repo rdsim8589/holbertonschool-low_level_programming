0414b5b0864948ed38188bdbea40046cfa0e9595
Richard Sim <rdsim8589@gmail.com>
2016-12-01 23:29:40 +0000

    accounting if head == NULL, and if current index node is NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,6 +3,7 @@
  * insert_nodeint_at_index - insert a new node after the given index
  * @head: a pointer to a pointer to the start of the linked list
  * @index: the index in which a new node is inserted after the index
+ * @n: an int value to place within the node
  *
  * Return: the address of the new node or
  * NULL if it fails, or unable to add a new node.
@@ -12,10 +13,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 	listint_t *new_node, *current;
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+	i = 0u;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	i = 0u;
 	current = *head;
 	while (i < index - 1)
 	{
@@ -25,9 +28,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		}
 		else
 		{
-			return(NULL);
+			return (NULL);
 		}
 		i++;
+	}
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
 	}
 	new_node->n = n;
 	new_node->next = current->next;
