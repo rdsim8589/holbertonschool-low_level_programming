628b8716f695cc903e820264610e077b30b8db77
Richard Sim <rdsim8589@gmail.com>
2016-12-01 23:19:00 +0000

    considering if **head is NULL)

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -7,11 +7,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *hold;
 
-	while (*head != NULL)
+	while (head != NULL && *head != NULL)
 	{
 		hold = *head;
 		*head = (*head)->next;
 		free(hold);
 	}
-	*head = NULL;
 }
