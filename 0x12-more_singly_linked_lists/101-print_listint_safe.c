8d1413a621b8a094dcd978a0725f5573438b19e6
Richard Sim <rdsim8589@gmail.com>
2016-12-03 20:54:38 +0000

    pushing assignment 12

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,85 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list, only touch each node once
+ * @head: a const pointer to the first node
+ *
+ * Return: the number of nodes in a list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *current;
+	list_check_t *head_2, *current_2;
+	size_t i;
+
+	i = 0;
+	current = head;
+	head_2 = NULL;
+	current_2 = head_2;
+	while(current != NULL)
+	{
+		while(current_2 != NULL)
+		{
+			if (current == current_2->ptr)
+			{
+				free_listint3 (head_2);
+				printf("-> [%p] %d",
+				       (void *)current, current->n);
+				return (i);
+			}
+			current_2 = current_2->next;
+		}
+
+		printf("[%p] %d\n", (void*)current, current->n);
+		add_nodeint_end_2(&head_2, (void *)current);
+		current = current->next;
+		current_2 = head_2;
+		i++;
+	}
+	return(i);
+}
+/**
+ * free_listint3 - frees a listint_t list
+ * @head: a pointer to the head of a linked list
+ */
+void free_listint3(list_check_t *head)
+{
+	list_check_t *hold;
+
+	while (head != NULL)
+	{
+		hold = head;
+		head = head->next;
+		free(hold);
+	}
+}
+/**
+ * add_nodeint_end_2 - add a node at the end of the linked list
+ * @head: a pointer to a pointer to the head of struct listint_t
+ * @n: an int value to fill in the node
+ *
+ * Return: the address of the new node, or NULL if failed
+ */
+list_check_t *add_nodeint_end_2(list_check_t **head, void* ptr)
+{
+	list_check_t *new, *current;
+
+	/* creating a node */
+	new = malloc(sizeof(list_check_t));
+	if (new == NULL)
+		exit(98);
+	new->ptr = ptr;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new;
+	}
+	return (new);
+}
