097951a43e759d3dd74cadd5021b4784b033176c
Richard Sim <rdsim8589@gmail.com>
2016-12-03 21:05:54 +0000

    added new line to printf('-> [%p] %d'

--- 0x12-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,31 +11,31 @@ size_t print_listint_safe(const listint_t *head)
 	list_check_t *head_2, *current_2;
 	size_t i;
 
-	i = 0;
+	i = 0u;
 	current = head;
 	head_2 = NULL;
 	current_2 = head_2;
-	while(current != NULL)
+	while (current != NULL)
 	{
-		while(current_2 != NULL)
+		while (current_2 != NULL)
 		{
 			if (current == current_2->ptr)
 			{
-				free_listint3 (head_2);
-				printf("-> [%p] %d",
+				free_listint3(head_2);
+				printf("-> [%p] %d\n",
 				       (void *)current, current->n);
 				return (i);
 			}
 			current_2 = current_2->next;
 		}
 
-		printf("[%p] %d\n", (void*)current, current->n);
+		printf("[%p] %d\n", (void *)current, current->n);
 		add_nodeint_end_2(&head_2, (void *)current);
 		current = current->next;
 		current_2 = head_2;
 		i++;
 	}
-	return(i);
+	return (i);
 }
 /**
  * free_listint3 - frees a listint_t list
@@ -55,11 +55,11 @@ void free_listint3(list_check_t *head)
 /**
  * add_nodeint_end_2 - add a node at the end of the linked list
  * @head: a pointer to a pointer to the head of struct listint_t
- * @n: an int value to fill in the node
+ * @ptr: a pointer a node in the main linkedlist
  *
  * Return: the address of the new node, or NULL if failed
  */
-list_check_t *add_nodeint_end_2(list_check_t **head, void* ptr)
+list_check_t *add_nodeint_end_2(list_check_t **head, void *ptr)
 {
 	list_check_t *new, *current;
 
