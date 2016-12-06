e6cac7d0102012cf02ef25aad11ba09e2f2a739f
Richard Sim <rdsim8589@gmail.com>
2016-12-06 02:10:52 +0000

    changed 1-print_binary to account for unsigned long int max

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -7,23 +7,13 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 		_putchar('0');
-	else
-		recursive_print_binary(n, 1);
-}
-
-/**
- * recursive_print_binary - print out the binary representation of a number
- * @n: number to be converted to Binary
- */
-
-void recursive_print_binary(unsigned long int n, unsigned long int multiple_two)
-{
-	if (multiple_two <= n)
+	if (n > 0)
 	{
-		recursive_print_binary(n, multiple_two << 1);
-		if ((n & multiple_two) != 0)
+		print_binary(n>>1);
+		if ((n & 1) != 0)
 			_putchar('1');
 		else
 			_putchar('0');
 	}
+
 }
