6ff0e23eba6330f7dd665149da76ee57f26fda03
Richard Sim <rdsim8589@gmail.com>
2016-12-06 01:47:33 +0000

    removing _putchar helper

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -27,15 +27,3 @@ void recursive_print_binary(unsigned long int n, unsigned long int multiple_two)
 			_putchar('0');
 	}
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
