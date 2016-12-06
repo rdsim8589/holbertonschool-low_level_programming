78f689d912a132f9db19a5ca7520a3e072c3f42e
Richard Sim <rdsim8589@gmail.com>
2016-12-06 04:40:10 +0000

    completed 100-get_endiannes.c

---
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * get_endianness - check the endianness
+ *
+ * Return: 1 if little endian and 0 if big endian
+ */
+int get_endianness(void)
+{
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
+}
