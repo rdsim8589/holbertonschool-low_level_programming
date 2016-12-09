8b4cf81726b019f982f2b708cdc12aa8aa69cb0b
Richard Sim <rdsim8589@gmail.com>
2016-12-09 22:42:07 +0000

    changed the description for 1-print_rev_recursion.c

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _print_rev_recursion - prints a string, in reverse.
+ * _print_rev_recursion - prints a string, in reverse using recursion.
  * @s: a pointer to a string
  */
 
