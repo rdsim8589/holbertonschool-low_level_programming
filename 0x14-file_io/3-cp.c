fc6142cf10e54f03a602dc4e8f680c992ae5833f
Richard Sim <rdsim8589@gmail.com>
2016-12-09 00:50:07 +0000

    0x14-3 complete :

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * main - copies the content of a file to another
+ * @argc: the number of the arguments given
+ * @argv: a pointer to the name of the files
+ *
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv)
+{
+	int file_to, file_from, r_file, w_file, c_file;
+	mode_t mode;
+	char buf[BUF_SIZE];
+
+	mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	if (argv[1] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+			, argv[1]), exit(98);
+	if (argv[2] == NULL)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+			, argv[1]), exit(98);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n"
+			, argv[2]), exit(99);
+	}
+	r_file = read(file_from, buf, BUF_SIZE);
+	if (r_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n"
+			, argv[1]), exit(98);
+	if (r_file > 0)
+	{
+		w_file = write(file_to, buf, r_file);
+		if (w_file == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+	c_file = close(file_from);
+	if (c_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_from), exit(100);
+	c_file = close(file_to);
+	if (c_file == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", file_to), exit(100);
+	return (0);
+
+}
