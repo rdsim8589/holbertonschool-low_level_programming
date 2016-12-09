a22bc616975c47d8ac6d4bbd6a623d692bf46bc7
Richard Sim <rdsim8589@gmail.com>
2016-12-09 22:27:20 +0000

    changed pointer notation to array

--- 0x04-pointers_arrays_strings/3-puts.c
+++ 0x04-pointers_arrays_strings/3-puts.c
@@ -9,9 +9,7 @@ void _puts(char *str_ptr)
 {
 	int n;
 
-	for (n = 0; *(str_ptr + n) != '\0'; n++)
-	{
-		_putchar(*(str_ptr + n));
-	}
+	for (n = 0; str_ptr[n] != '\0'; n++)
+		_putchar(str_ptr[n]);
 	_putchar('\n');
 }
