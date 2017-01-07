43df9370098e3ff98f3521089af8b256cbfcd347
Richard Sim <rdsim8589@gmail.com>
2017-01-07 13:16:55 -0800

    completed assignment 6

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,2 +1,17 @@
-#include "list.h"
 #include "lists.h"
+/**
+ * sum_dlistint - sum of all the data (n) of a dlistint_t
+ * @head: pointer to dlistint_t linkedlist
+ *
+ * Return: the sum of all the data (n) of a dlistint_t
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	while (head->prev != NULL)
+		head = head->prev;
+	for (sum = 0; head->next != NULL; sum += head->n)
+		head = head->next;
+	return (sum);
+}
