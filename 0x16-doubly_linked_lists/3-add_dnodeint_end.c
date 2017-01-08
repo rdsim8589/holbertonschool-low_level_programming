b8268045f8e780acdbd390edc5d4e9308efc487f
Richard Sim <rdsim8589@gmail.com>
2017-01-07 18:42:21 -0800

    completed assigment

--- 0x16-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x16-doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,7 +23,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 			current = current->next;
 	else
 		return (*head = node);
-
+	node->prev = current;
 	current->next = node;
 	return (node);
 }
