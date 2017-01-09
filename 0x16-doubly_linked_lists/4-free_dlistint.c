c394f4d41175ccfa63bfd6ec0e37038a8a28a803
Richard Sim <rdsim8589@gmail.com>
2017-01-08 18:33:16 -0800

    changing documenting style

--- 0x16-doubly_linked_lists/4-free_dlistint.c
+++ 0x16-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * free_dlistint: free a linked list
+ * free_dlistint - free a linked list
  * @head: the head to dilistint_t list
  */
 void free_dlistint(dlistint_t *head)
