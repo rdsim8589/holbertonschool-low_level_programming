b07c745a17308a359bed89af90a23aed10f88be7
Richard Sim <rdsim8589@gmail.com>
2017-01-29 12:17:35 -0800

    changed approach

--- 0x03-more_functions_nested_loops/8-print_square.c
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -9,17 +9,12 @@ void print_square(int size)
 {
 	int i, j;
 
-	if (size > 0)
+	for (i = size; i > 0; i--)
 	{
-		for (i = 0; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-				_putchar('#');
-			_putchar('\n');
-		}
-	}
-	else
-	{
+		for (j = 0; j < size; j++)
+			_putchar('#');
 		_putchar('\n');
 	}
+	if (size == 0)
+			_putchar('\n');
 }
