ae7ed0e748f999250a2fd0e2b19441647848070b
Richard Sim <rdsim8589@gmail.com>
2017-02-01 15:10:57 -0800

    updating assignment

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str_holder = va_arg(strings, char*);
 		if (separator != NULL)
-		{
+ 		{
 			if (str_holder != NULL)
 			{
 				if (i == n - 1)
