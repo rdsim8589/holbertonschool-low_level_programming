171f7d599ec4d75218260e9ecfd8fdbd14fc030f
Richard Sim <rdsim8589@gmail.com>
2017-02-01 15:21:16 -0800

    changed implementation

--- 0x13-bit_manipulation/5-flip_bits.c
+++ 0x13-bit_manipulation/5-flip_bits.c
@@ -14,29 +14,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	flip = 0;
 	for (i = 0; i < (8 * sizeof(n)); i++)
 	{
-		if (get_bit(n, 0) != get_bit(m, 0))
+		if ((n & 1) != (m & 1))
 			flip++;
 		n = n >> 1;
 		m = m >> 1;
 	}
 	return (flip);
-}
-
-/**
- * get_bit - return the value of a bit at a given index
- * @n: a number
- * @index: the index if starting from the 0 place
- *
- * Return: the value of a bit at a given index or -1 if it fails.
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	unsigned int compare;
-
-	if (index > 8 * sizeof(n))
-		return (-1);
-	compare = 1;
-	if (n & (compare << index))
-		return (1);
-	return (0);
 }
