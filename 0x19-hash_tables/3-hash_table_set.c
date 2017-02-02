5c4e848099100dabaca4e00d4de6b99bd0ccaedb
Richard Sim <rdsim8589@gmail.com>
2017-02-01 16:51:48 -0800

    moved to append at the front of the list

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -9,10 +9,10 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node, *scan_idx;
+	hash_node_t *node, **scan_idx;
 	unsigned long int index;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || ht == NULL)
 		return(0);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
@@ -22,16 +22,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = NULL;
 
 	index = key_index((const unsigned char *) key, ht->size);
-	scan_idx = ht->array[index];
+	scan_idx = &(ht->array[index]);
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
 	}
 	else
 	{
-		while (scan_idx->next != NULL)
-			scan_idx = scan_idx->next;
-		scan_idx->next = node;
+		node->next = *scan_idx;
+		*scan_idx = node;
 	}
 	return (1);
 }
