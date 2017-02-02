e43fd4013443edfc2f42c4f03e44d95fb2670d56
Richard Sim <rdsim8589@gmail.com>
2017-02-01 18:21:06 -0800

    made betty happy

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (key == NULL || ht == NULL)
-		return(0);
+		return (0);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
