893c908ec1bde7a88c3b5907a8273710cd6ce518
Richard Sim <rdsim8589@gmail.com>
2017-02-01 16:38:55 -0800

    account for NULL

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *scan_idx;
 	unsigned long int index;
 
-	if (key == NULL || value == NULL || ht == NUll)
+	if (key == NULL || value == NULL || ht == NULL)
 		return(0);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
