77790d8178e45a6cb41da8f1b21a7e5e568518e2
Richard Sim <rdsim8589@gmail.com>
2017-02-01 16:24:36 -0800

    account for null if key and value is null

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,6 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *scan_idx;
 	unsigned long int index;
 
+	if (key == NULL || value == NULL)
+		return(0);
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
