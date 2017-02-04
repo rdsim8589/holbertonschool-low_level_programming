9633f5525669093a5d462509a874dcea7cd1c24a
Richard Sim <rdsim8589@gmail.com>
2017-02-04 12:14:50 -0800

    strdup the value as the node struct stores a char*

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,20 +11,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node, *scanner_idx;
 	unsigned long int index;
+	char *value_cpy;
 
 	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
-
+	value_cpy = strdup(value);
+	if (value_cpy == NULL)
+		return (0);
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
+	{
+		free(value_cpy);
 		return (0);
+	}
 	node->key = (char *) key;
-	node->value = (char *) value;
+	node->value = value_cpy;
 	node->next = NULL;
-
 	index = key_index((const unsigned char *) key, ht->size);
 	scanner_idx = ht->array[index];
-
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
