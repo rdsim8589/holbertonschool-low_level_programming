0318336759678565ca240bb96bc4e83c8f3d9362
Richard Sim <rdsim8589@gmail.com>
2017-02-05 15:42:23 -0800

    added function for alternative versions of task 3 and 5

--- 0x19-hash_tables/hash_tables.h
+++ 0x19-hash_tables/hash_tables.h
@@ -38,4 +38,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 hash_node_t *chk_ht_node(const char *key, const char *val);
+void hash_table_delete(hash_table_t *ht);
+hash_node_t* scan_ht_check(hash_node_t* scanner_idx, const char *key);
+int prepend_node(hash_node_t** head, hash_node_t* node);
 #endif
