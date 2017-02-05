fac0dd65b824c1ed9fbdfc373371f5f5975df25f
Richard Sim <rdsim8589@gmail.com>
2017-02-05 15:43:36 -0800

    updated 5-main to acccount for updating value and collisions

--- 0x19-hash_tables/mains/5-main.c
+++ 0x19-hash_tables/mains/5-main.c
@@ -13,13 +13,22 @@ int main(void)
 	hash_table_t *ht;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "c", "fun");
-	hash_table_set(ht, "python", "awesome");
-	hash_table_set(ht, "Jennie", "and Jay love asm");
-	hash_table_set(ht, "N", "queens");
-	hash_table_set(ht, "Asterix", "Obelix");
-	hash_table_set(ht, "Betty", "Holberton");
-	hash_table_set(ht, "98", "Battery Street");
+	hash_table_set(ht, "betty", "holberton");
+	hash_table_set(ht, "hetairas", "value 1");
+	hash_table_set(ht, "mentioner", "value 2");
+	hash_table_set(ht, "heliotropes", "value 3");
+	hash_table_set(ht, "neurospora", "value 4");
+	hash_table_set(ht, "stylist", "value 5");
+	hash_table_set(ht, "serafins", "value 6");
+	hash_table_set(ht, "subgenera", "value 7");
+	hash_table_set(ht, "synaphea", "value 8");
+	hash_table_set(ht, "joyful", "value 9");
+	hash_table_set(ht, "redescribed", "value 10");
+	hash_table_set(ht, "urites", "value 11");
+	hash_table_set(ht, "dram", "value 12");
+	hash_table_set(ht, "vivency", "value 13");
+	hash_table_set(ht, "serafins", "replaced!");
 	hash_table_print(ht);
+	hash_table_delete(ht);
 	return (EXIT_SUCCESS);
 }
