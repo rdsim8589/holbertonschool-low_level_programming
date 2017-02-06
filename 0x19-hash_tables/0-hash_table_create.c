4cc92f67391e8a4b5dd1e776ce9cb20dc9eef62c
Richard Sim <rdsim8589@gmail.com>
2017-02-06 08:33:25 -0800

    only need to account for when size is 0

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 	unsigned long int i;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
 	hash = malloc(sizeof(hash_table_t));
 	if (hash == NULL)
