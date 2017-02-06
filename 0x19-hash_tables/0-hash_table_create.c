57b854327222d9cec6839ca30f0076134c80c48f
Richard Sim <rdsim8589@gmail.com>
2017-02-06 08:24:43 -0800

    changed i++ to ++i

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; ++i)
 		hash->array[i] = NULL;
 	hash->size = size;
 	return (hash);
