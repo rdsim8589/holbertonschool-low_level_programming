2f3b8bcccd6cbe86ce73b28bbe9d2e1a3d7b599c
Richard Sim <rdsim8589@gmail.com>
2017-02-19 16:37:13 -0800

    fixed typo for ary_divider

--- 0x1A-sorting_algorithms/sort.h
+++ 0x1A-sorting_algorithms/sort.h
@@ -24,7 +24,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 /* the actual quick sort */
 void rl_quick_sort(int *array, int start, int end, size_t size);
-int ary_divivder(int *array, int start, int end, size_t size);
+int ary_divider(int *array, int start, int end, size_t size);
 
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
