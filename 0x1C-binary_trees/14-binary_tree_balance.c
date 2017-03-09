9c2392b414cf0ccfe2c3b387293895bc7889c23e
Richard Sim <rdsim8589@gmail.com>
2017-03-09 11:17:01 -0800

    completed finding the tree balance

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -1,1 +1,18 @@
 #include "binary_trees.h"
+#include "9-binary_tree_height.c"
+/**
+ * binary_tree_balance - measures the balance factor
+ * tree: pointer to the node
+ *
+ * Return: the balance factor or 0 if NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int right, left;
+
+	if (tree == NULL)
+		return (0);
+	right = (int) tree_height(tree->right);
+	left = (int) tree_height(tree->left);
+	return (left - right);
+}
