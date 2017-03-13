e4568163551e345448b0dfb9d1527c842fee202b
Richard Sim <rdsim8589@gmail.com>
2017-03-12 19:17:46 -0700

    created a script that returns the uncle of a node

--- 0x1C-binary_trees/18-binary_tree_uncle.c
+++ 0x1C-binary_trees/18-binary_tree_uncle.c
@@ -1,1 +1,19 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_uncle - returns the node's uncle
+ * @node: pointer to the node to find the uncle of
+ *
+ * Return: pointer to the uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node != NULL && node->parent != NULL &&
+	    node->parent->parent != NULL)
+	{
+		if (node->parent->parent->right == node->parent)
+			return (node->parent->parent->left);
+		if (node->parent->parent->left == node->parent)
+			return (node->parent->parent->right);
+	}
+	return (NULL);
+}
