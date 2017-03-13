5586a5636462151f1208db4230e12f5818553313
Richard Sim <rdsim8589@gmail.com>
2017-03-12 19:16:25 -0700

    condense height to one function

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -1,25 +1,5 @@
 #include "binary_trees.h"
 /**
- * tree_height - returns the tree height + 1
- * @tree: pointer to the tree
- *
- * Return: the tree height + 1
- */
-size_t tree_height(const binary_tree_t *tree)
-{
-	size_t left_tree, right_tree;
-
-	if (tree == NULL)
-		return (0);
-	left_tree = tree_height(tree->left);
-	right_tree = tree_height(tree->right);
-	if (left_tree > right_tree)
-		return (left_tree + 1);
-	else
-		return (right_tree + 1);
-}
-
-/**
  * binary_tree_height - wrapper for tree height
  * @tree: tree node
  *
@@ -27,7 +7,17 @@ size_t tree_height(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
-	return (tree_height(tree) - 1);
+	if (tree->right == NULL && tree->left == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
