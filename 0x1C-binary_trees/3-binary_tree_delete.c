5f15fe00039cd9aec59f596acf9aa56393bf2ffd
Richard Sim <rdsim8589@gmail.com>
2017-03-20 09:54:00 -0700

    account for null check when deleting

--- 0x1C-binary_trees/3-binary_tree_delete.c
+++ 0x1C-binary_trees/3-binary_tree_delete.c
@@ -5,11 +5,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return
 	if (tree->left != NULL)
 		binary_tree_delete(tree->left);
 	if (tree->right != NULL)
 		binary_tree_delete(tree->right);
 	free(tree);
-
-
 }
