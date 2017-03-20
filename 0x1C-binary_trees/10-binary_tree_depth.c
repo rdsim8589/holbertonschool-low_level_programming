7bd7656e226db3da880d752000e91e7dfea8e90b
Richard Sim <rdsim8589@gmail.com>
2017-03-20 09:55:25 -0700

    account for null check

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -3,12 +3,14 @@
  * binary_tree_depth - gets the depth of the node
  * @node: the node to check
  *
- * Return: the depth of a node
+ * Return: the depth of a node, or 0 otherwise
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
 	size_t depth;
 
+	if (node == NULL)
+		return (0);
 	for (depth = 0; node->parent != NULL; depth++)
 		node = node->parent;
 	return (depth);
