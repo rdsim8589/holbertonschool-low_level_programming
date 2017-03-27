de8733f354fc99b32fc2ce8f2101130c87ebf3fa
Richard Sim <rdsim8589@gmail.com>
2017-03-27 12:50:27 -0700

    account for a way if the tree is balanced

--- 0x1C-binary_trees/120-binary_tree_is_avl.c
+++ 0x1C-binary_trees/120-binary_tree_is_avl.c
@@ -1,24 +1,48 @@
 #include "binary_trees.h"
 #include "110-binary_tree_is_bst.c"
-#include "9-binary_tree_height.c"
 /**
- * height_diff_by_one - if the height of the tree differs by one
+ * binary_tree_height - finds the tree's height
+ * @tree: tree node
+ *
+ * Return: the height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
+ * if_bst_balance - if the height of the tree differs by one at most one
  * @tree: pointer to the tree
  *
  * Return: 1 tree differs by at most one, 0 otherwise
  */
-int height_diff_by_one(const binary_tree_t *tree)
+int if_bst_balance(const binary_tree_t *tree)
 {
 	int h_left, h_right, diff_left, diff_right;
 
 	if (tree == NULL)
 		return (1);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
 	if ((h_left - h_right) >= -1 && (h_left - h_right) <= 1)
 	{
-		diff_left = height_diff_by_one(tree->left);
-		diff_right = height_diff_by_one(tree->right);
+		diff_left = if_bst_balance(tree->left);
+		diff_right = if_bst_balance(tree->right);
 		return (diff_left && diff_right);
 	}
 	return (0);
@@ -32,5 +56,5 @@ int height_diff_by_one(const binary_tree_t *tree)
  */
 int binary_tree_is_avl(const binary_tree_t *tree)
 {
-	return (height_diff_by_one(tree) && binary_tree_is_bst(tree));
+	return (if_bst_balance(tree) && binary_tree_is_bst(tree));
 }
